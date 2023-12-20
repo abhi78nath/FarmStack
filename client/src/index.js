788a23ba49b19a9f70ff824f103ac655582f1038
abhishek nath <abhishek78nath@gmail.com>
2023-12-20 17:32:03 +0530

    added manifest,service worker for pwa

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+// import { I18nextProvider } from 'react-i18next';
+// import i18n from './i18n';
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(
   <ChakraProvider>
     <BrowserRouter>
       <ChatProvider>
-        <I18nextProvider i18n={i18n}>
+        {/* <I18nextProvider i18n={i18n}> */}
           <App />
-        </I18nextProvider>
+        {/* </I18nextProvider> */}
       </ChatProvider>
     </BrowserRouter>
   </ChakraProvider>,
   document.getElementById("root")
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
